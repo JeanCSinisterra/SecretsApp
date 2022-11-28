122f78a475a5d7e9d95a0747353b8d1977e5e488
JeanCSinisterra <84473537+JeanCSinisterra@users.noreply.github.com>
2022-11-28 00:01:14 -0500

    Last Change

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
-const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
 
@@ -38,22 +39,26 @@ app.get("/register", function (req, res){
 });
 
 app.post("/register", function(req, res){
-    const newUser = new User({
-        email: req.body.username,
-        password: md5(req.body.password)
-    });
-    newUser.save(function(err){
-        if(err){
-           console.log(err);
-        } else {
-            res.render("secrets");
-        }
+    
+    bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+        // Store hash in your password DB.
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        });
+        newUser.save(function (err) {
+            if (err) {
+                console.log(err);
+            } else {
+                res.render("secrets");
+            }
+        });
     });
 });
 
 app.post("/login", function(req, res){
     const username = req.body.username;
-    const password = md5(req.body.password);
+    const password = req.body.password;
 
     //check if the userName is already registered:
     User.findOne({email: username}, function(err, foundUser){
@@ -61,13 +66,15 @@ app.post("/login", function(req, res){
             console.log(err);
         } else {
             if (foundUser){
-                if (foundUser.password === password){
+                bcrypt.compare(password, foundUser.password, function (err, result) {
+                if  (result === true) {
                     res.render("secrets");
+                }
+                });
                 }
             }
         }
-    });
-});
+    )});
 
 
 
