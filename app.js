01905cb2599e2ed5077d0a0f5b4d77acd04601ca
JeanCSinisterra <84473537+JeanCSinisterra@users.noreply.github.com>
2022-11-29 08:02:35 -0500

    changes to solve the scope issue

--- app.js
+++ app.js
@@ -69,7 +69,8 @@ passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
     callbackURL: "http://localhost:3000/auth/google/secrets",
-    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
+    scope: ["openid", "profile", "email"]
 },
     function (accessToken, refreshToken, profile, cb) {
         console.log(profile);
